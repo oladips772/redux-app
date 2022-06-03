bd0d69364f104fc6a6c0d9d66d023f9d193ebee3
oladips <oladips200@gmail.com>
2022-06-03 06:01:15 +0000

    added and configured redux

---
+++ src/Redux/features/counter/counterSlice.js
@@ -0,0 +1,22 @@
+/** @format */
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  count: 0,
+};
+
+const counterSlice = createSlice({
+  name: "count",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.count += 1;
+    },
+    decrement: (state) => {
+      state.count -= 1;
+    },
+  },
+});
+
+export const { increment, decrement } = counterSlice.actions;
+export default counterSlice.reducer;
---
+++ src/Redux/store.js
@@ -0,0 +1,9 @@
+/** @format */
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./features/counter/counterSlice";
+
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+});
